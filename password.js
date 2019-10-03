3509d6aa1675f9bdb6ea9c1938c50f28841fa6a6
nerophd <56097516+nerophd@users.noreply.github.com>
2019-10-03 15:17:56 +0930

    Delete password.js

--- password.js
+++
@@ -1,7 +0,0 @@
-    function checkPswd() {
-      var confirmPassword = "YEEZY";
-      var password = document.getElementById("pswd").value;
-      if (password == confirmPassword) {
-        window.location = "welkom.html";
-      }
-    }
